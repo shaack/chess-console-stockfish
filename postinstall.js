3db620fc835b77af44eec9d0be1fdd0d6987f01b
Stefan Haack <shaack@gmail.com>
2018-08-01 23:36:10 +0200

    initial commit

---
+++ postinstall.js
@@ -0,0 +1,24 @@
+/**
+ * Author and copyright: Stefan Haack (https://shaack.com)
+ * License: MIT, see file 'LICENSE'
+ */
+
+const WebModuleLinker = require("web-module-linker")
+
+const linker = new WebModuleLinker(__dirname)
+
+linker.symlinkModuleSrc("svjs-app")
+linker.symlinkModuleSrc("svjs-utils")
+linker.symlinkModuleSrc("svjs-i18n")
+linker.symlinkModuleSrc("svjs-message-broker")
+linker.symlinkModuleSrc("svjs-svg")
+linker.symlinkModuleSrc("svjs-observe")
+linker.symlinkModuleSrc("svjs-audio")
+
+linker.symlinkModuleSrc("cm-chessboard")
+linker.symlinkModuleSrc("cm-bootstrap-modal")
+linker.symlinkModuleSrc("cm-chesstools")
+
+linker.symlinkModuleSrc("chess-console")
+
+linker.symlinkModuleSrc("bootstrap-show-modal", "bootstrap-show-modal.js")
---
+++ src/chess-console-stockfish/StockfishGameControl.js
@@ -0,0 +1,11 @@
+/**
+ * Author and copyright: Stefan Haack (https://shaack.com)
+ * Repository: https://github.com/shaack/chess-console-stockfish
+ * License: MIT, see file 'LICENSE'
+ */
+
+import {ChessConsolePlayer} from "chess-console/src/chess-console/ChessConsolePlayer.js"
+
+export class StockfishPlayer extends ChessConsolePlayer {
+
+}
