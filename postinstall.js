461d73ba9d2d42e7f878c0cd58cc647a93ce2eb0
Stefan Haack <shaack@gmail.com>
2019-08-10 15:11:52 +0200

    update with cm-web-modules

--- postinstall.js
+++ postinstall.js
@@ -3,22 +3,11 @@
  * License: MIT, see file 'LICENSE'
  */
 
-const WebModuleCurator = require("web-module-curator")
+const LibraryManager = require("cm-web-modules/src/LibraryManager.js")
+const manager = new LibraryManager(__dirname)
 
-const curator = new WebModuleCurator(__dirname)
+manager.addProject("cm-web-modules")
+manager.addProject("cm-chessboard")
+manager.addProject("cm-chesstools")
 
-curator.addModule("svjs-app")
-curator.addModule("svjs-utils")
-curator.addModule("svjs-i18n")
-curator.addModule("svjs-message-broker")
-curator.addModule("svjs-svg")
-curator.addModule("svjs-observe")
-curator.addModule("svjs-audio")
-
-curator.addModule("cm-chessboard")
-curator.addModule("cm-chesstools")
-
-curator.addModule("chess-console")
-
-curator.addModule("bootstrap-show-modal", "src", "bootstrap-show-modal.js")
-
+manager.addProject("bootstrap-show-modal", "src", "bootstrap-show-modal.js")
